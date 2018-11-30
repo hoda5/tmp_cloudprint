0d15142d90302bbec9bd8c2c7d21407120f73652
hoda5 <josefernandotolentino@gmail.com>
2018-11-30 12:06:48 -0200

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var http = require('http');
+var cors = require('cors');
+var expressvar = express();
+
+expressvar.use(function (req, res, next) {
+    var origin = req.header('origin') || "localhost:8282"
+    res.setHeader('Access-Control-Allow-Headers', 'accept, authorization, content-type, origin, x-requested-with');
+    res.setHeader('Access-Control-Allow-Methods', 'POST');
+    res.setHeader('Access-Control-Allow-Origin', "https://www.google.com");
+    next();
+});
+
+// expressvar.use(cors());
+expressvar.use(express.static('./public'));
+var app = http.createServer(expressvar);
+app.listen(8282);
